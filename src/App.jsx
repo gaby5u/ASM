42bcfbff49622ff0db8b810244e4be6e6d7a438e
gaby5u <gabrielaoloeri@gmail.com>
2025-05-29 16:40:55 +0300

    Create navbar

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Navbar from "./components/Navbar.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import About from "./pages/About.jsx";
+import News from "./pages/News.jsx";
+import Acquisitions from "./pages/Acquisitions.jsx";
+import Transparency from "./pages/Transparency.jsx";
+import Contact from "./pages/Contact.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/despre" element={<About />} />
+          <Route path="/noutăți" element={<News />} />
+          <Route path="/achiziții" element={<Acquisitions />} />
+          <Route path="/transparență" element={<Transparency />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Acquisitions.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Acquisitions = () => {
+  return (
+    <div>Acquisitions</div>
+  )
+}
+
+export default Acquisitions
