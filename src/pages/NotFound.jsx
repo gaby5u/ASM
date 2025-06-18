b906107e46b1eb87e352bd2fa8d7499febe58bc2
gaby5u <gabrielaoloeri@gmail.com>
2025-06-18 21:14:35 +0300

    Create 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import Title from "../components/Title";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="bg-blue-100 flex flex-col items-center justify-center py-[25vh]">
+      <Title title="404 Page" customClassName="my-0" />
+      <h2 className="font-rubik font-semibold text-xl text-blue-500 mt-4 lg:text-3xl">Această pagină nu exista</h2>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
